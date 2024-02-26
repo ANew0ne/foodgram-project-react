6ea83d44374d8b355f2f5de7a303904a2c1bfb78
Denis Poddubny <dgreenpie@gmail.com>
2024-02-26 21:18:35 +0300

    isort mistake fixed

--- backend/users/models.py
+++ backend/users/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
 
-
 EMAIL_MAX_LENGTH = 254
 STRING_MAX_LENGTH = 150
 
